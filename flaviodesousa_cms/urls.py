59176ea2e516ffd8e912beeac29df21d87847fce
Flávio de Sousa <fl@viodesousa.com>
2012-09-23 11:12:56 -0300

    filer plugins enabled

--- flaviodesousa_cms/urls.py
+++ flaviodesousa_cms/urls.py
@@ -12,5 +12,4 @@ if settings.DEBUG:
     urlpatterns = patterns('',
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'', include('django.contrib.staticfiles.urls')),
-) + urlpatterns
+    url(r'', include('django.contrib.staticfiles.urls')),) + urlpatterns
