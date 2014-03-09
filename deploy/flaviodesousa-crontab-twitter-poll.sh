2c4d190b83015bb3358996496d7537ec49e236ea
Flavio de Sousa <fl@viodesousa.com>
2014-03-08 22:27:08 -0300

    env passing

--- deploy/flaviodesousa-crontab-twitter-poll.sh
+++ deploy/flaviodesousa-crontab-twitter-poll.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 . /var/python/env/flaviodesousa/bin/activate
-CMS_FLAVIODESOUSA_ADMIN_URL=^admin/
-CMS_DJANGO_SECRET_KEY=irrelevant
+
+CMS_FLAVIODESOUSA_ADMIN_URL=x \
+CMS_DJANGO_SECRET_KEY=x \
 /var/www/flaviodesousa/manage.py poll_twitter
