929f9693cbadcf6ad67b802f0c05d0cf52e2f0bc
Flavio de Sousa <fl@viodesousa.com>
2014-03-08 23:38:33 -0300

    mypygmentize now allows passing arguments to pygmentize

--- code_fragments/mypygmentize.sh
+++ code_fragments/mypygmentize.sh
@@ -1,13 +1,16 @@
 #! /bin/sh
 
-if [ -z "$1" -o ! -f $1 ]; then
+source_file=$1
+shift
+
+if [ -z "$source_file" -o ! -f $source_file ]; then
   echo Please, give me a valid filename to pygmentize
   exit 1
 fi
 
 echo copy output into HTML text of article
 echo -------------------------------------
-pygmentize -f html $1 | xclip -selection clipboard
+pygmentize -f html $* $source_file | xclip -selection clipboard
 xclip -selection clipboard -o
 echo -------------------------------------------
 echo HTML above must already be in the clipboard
